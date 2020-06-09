876ee9b60054996480915a5567ed0e77b4994c76
dustinkredmond <42328885+dustinkredmond@users.noreply.github.com>
2020-06-09 12:06:05 -0400

    Update comments
    Pandas no longer automatically converts spaces to underscores in column names.

--- excel_to_db.py
+++ excel_to_db.py
@@ -20,6 +20,8 @@ if len(excel_sheets) > 0:
     for sheet in excel_sheets:
         df = pd.read_excel(sheet)
         conn = sqlite3.connect("{}.db".format(sheet[:sheet.rindex('.')]))
+        # NOTE: Pandas does not convert column name spaces to underscore by default
+        # anymore, some people may find that they want this. Do that here.
         df.to_sql(name=sheet[:sheet.rindex('.')], con=conn)
 else:
     print("Please supply the path to an Excel sheet as an argument")
