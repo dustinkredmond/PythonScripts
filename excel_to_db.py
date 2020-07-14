25249bc589bf9e069032f32306ecc673692b7b81
dustinkredmond <42328885+dustinkredmond@users.noreply.github.com>
2020-07-14 15:27:57 -0400

    Refactor to change column text formatting; added better comments
    - Pandas no longer converts spaces to underscores in column names, so do that.
    - For better consistency, make all column names uppercase.
    - In the future we should also sanitize any invalid column name characters for SQLite (need to look these chars up)

--- excel_to_db.py
+++ excel_to_db.py
@@ -1,27 +1,52 @@
 import pandas as pd
+from os import access, R_OK
+from os.path import isfile
 import sqlite3
 import sys
+
 '''
  Simple script to convert an Excel spreadsheet into a
  SQLite3 database. This makes complex queries a breeze.
- 
- Expects one or more paths to Excel workbooks as command line arguments
- Requires pandas libary, you must run:
-     $ pip install pandas
 '''
 
 excel_sheets = []
 if len(sys.argv) > 1:
-    sys.argv.remove(0)
+    sys.argv.remove(0)  # argv[0] will be program name
     for arg in sys.argv:
         excel_sheets.append(arg)
 
-if len(excel_sheets) > 0:
-    for sheet in excel_sheets:
-        df = pd.read_excel(sheet)
-        conn = sqlite3.connect("{}.db".format(sheet[:sheet.rindex('.')]))
-        # NOTE: Pandas does not convert column name spaces to underscore by default
-        # anymore, some people may find that they want this. Do that here.
+# For testing
+excel_sheets.append("test.xls")
+
+if len(excel_sheets) == 0:
+    print("Please supply the path to an Excel sheet as an argument.")
+    sys.exit(-1)
+
+# Read each argument as a file path
+for sheet in excel_sheets:
+    # Check that arguments were files, and can read
+    if not isfile(sheet) or not access(sheet, R_OK):
+        print("File: {} unable to access or is not a"
+              " valid file path. Skipping.".format(sheet))
+        continue
+
+    # Read excel file into pandas DataFrame
+    df = pd.read_excel(sheet)
+    # Get each column name as string for normalization
+    columns = list(map(str, df.columns))
+    # Replace each space with underscore in columns
+    columns = [i.replace(" ", "_") for i in columns]
+    # Make each column name uppercase
+    columns = [i.upper() for i in columns]
+    # Replace columns in original DataFrame with pretty column names
+    df.columns = columns
+
+    # Create SQLite database with name of excel file (sans file extension)
+    conn = sqlite3.connect("{}.db".format(sheet[:sheet.rindex('.')]))
+    # Use DataFrame to_sql to add data from DataFrame to SQLite table
+    try:
         df.to_sql(name=sheet[:sheet.rindex('.')], con=conn)
-else:
-    print("Please supply the path to an Excel sheet as an argument")
+    except ValueError:
+        # to_sql raises ValueError if the table already exists
+        print("SQLite database already exists. Delete {} then try again."
+              .format("{}.db".format(sheet[:sheet.rindex('.')])))
